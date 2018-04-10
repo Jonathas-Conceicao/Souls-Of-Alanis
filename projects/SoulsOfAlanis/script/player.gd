29de8e0bda91b7d29f875bb3cdc080357b50717b
Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>
2018-04-10 00:00:46 -0300

    Jumpping animation fixed

--- projects/SoulsOfAlanis/script/player.gd
+++ projects/SoulsOfAlanis/script/player.gd
@@ -10,25 +10,30 @@ var motion = Vector2()
 
 func _physics_process(delta):
 	motion.y += GRAVITY
-		
+	if is_on_floor():
+		if $sprite.animation != "moving":
+			$sprite.play("idle")
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = -350
+			if $sprite.animation != "jumping":
+				$sprite.play("jumping")
+	elif $sprite.animation != "jumping" && $sprite.animation != "falling":
+		$sprite.play("falling")
 	if Input.is_action_pressed("ui_right"):
 		motion.x = SPEED	
 		$sprite.flip_h = false
-		$sprite.play("moving")
+		if $sprite.animation == "idle":
+			$sprite.play("moving")
 	elif Input.is_action_pressed("ui_left"):
 		motion.x = -SPEED
 		$sprite.flip_h = true
-		$sprite.play("moving")
+		if $sprite.animation == "idle":
+			$sprite.play("moving")
 	else:
 		motion.x = 0
-		$sprite.play("idle")
-	
-	if is_on_floor():
-		if Input.is_action_just_pressed("ui_up"):
-			motion.y = -350
-			$sprite.play("jumping")
-	else:
-		$sprite.play("jumping")
-		
+
 	motion = move_and_slide(motion, UP)
 
+func _on_sprite_animation_finished():
+	if $sprite.animation == "jumping":
+		$sprite.play("falling")
