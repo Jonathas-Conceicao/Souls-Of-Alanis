c8d71c669a3a59da2e7b34fbef71a3c2ed33a08c
Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>
2018-04-11 15:10:26 -0300

    Doing some work

---
+++ projects/JonathasTest101/script/player.gd
@@ -0,0 +1,40 @@
+extends KinematicBody2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+const UP = Vector2(0,-1)
+const GRAVITY = 10
+const SPEED = 150
+var motion = Vector2()
+
+func _physics_process(delta):
+	motion.y += GRAVITY
+	if is_on_floor():
+		if $sprite.animation != "moving":
+			$sprite.play("idle")
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = -350
+			$sprite.play("jumping")
+	elif $sprite.animation != "jumping" && $sprite.animation != "falling":
+		$sprite.play("falling")
+	if Input.is_action_pressed("ui_right"):
+		motion.x = SPEED	
+		$sprite.flip_h = false
+		if $sprite.animation == "idle":
+			$sprite.play("moving")
+	elif Input.is_action_pressed("ui_left"):
+		motion.x = -SPEED
+		$sprite.flip_h = true
+		if $sprite.animation == "idle":
+			$sprite.play("moving")
+	else:
+		if $sprite.animation == "moving":
+			$sprite.play("idle")
+		motion.x = 0
+
+	motion = move_and_slide(motion, UP)
+
+func _on_sprite_animation_finished():
+	if $sprite.animation == "jumping":
+		$sprite.play("falling")
