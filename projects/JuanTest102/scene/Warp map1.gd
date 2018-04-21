f3a5ff70378b0e388cc935a2752748da4ad48244
Asaki <asakizin@gmail.com>
2018-04-21 05:56:25 -0300

    back to previous scene is not working

---
+++ projects/JuanTest102/scene/Warp map1.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+func _physics_process(delta):
+	var bodies = get_overlapping_areas()
+	
+	for body in bodies:
+		if body.name == "Player":
+			print (bodies)
+			get_tree().change_scene("res://scene/World1.tscn")
