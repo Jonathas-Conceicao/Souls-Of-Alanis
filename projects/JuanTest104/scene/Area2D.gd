fe56c0980a3255222b07e36618e3661d199e2827
Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>
2018-06-06 01:44:22 -0300

    Cleaned personal projects from master

--- projects/JuanTest104/scene/Area2D.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-func _physics_process(delta):
-	var bodies = get_overlapping_bodies()
-		
-	for body in bodies:
-		if body.name == "Player":
-			get_tree().change_scene("res://scene/World1_1.tscn")
--- projects/JuanTest105/scene/Area2D.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-func _physics_process(delta):
-	var bodies = get_overlapping_bodies()
-		
-	for body in bodies:
-		if body.name == "Player" && Input.is_action_pressed("ui_attak"):
-			$Sprite.hide()
-			$Label.show()
