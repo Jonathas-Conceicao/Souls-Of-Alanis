33049e50906bef0e2aa556cb474c4df4a2ab5258
Lucas Eduardo Bretana <lebretana@inf.ufpel.edu.br>
2018-06-11 22:21:34 -0300

    Last minute modification

--- projects/SoulsOfAlanis/addons/godot-spawn-area/src/SpawnArea2D.gd
+++ projects/SoulsOfAlanis/addons/godot-spawn-area/src/SpawnArea2D.gd
@@ -10,47 +10,47 @@ export (bool)    			var FlyEnabled 	= false
 var MobCap = 0
 
 func _ready():
-	randomize()
+  randomize()
 
-	place_mob(pick_rand())
-	place_mob(pick_rand())
-	place_mob(pick_rand())
-	place_mob(pick_rand())
+  place_mob(pick_rand())
+  place_mob(pick_rand())
+  place_mob(pick_rand())
+  place_mob(pick_rand())
 
-	pass
+  pass
 
 func pick_rand():
-	return self.MobList[rand_range(0, self.MobList.size() -1)]
+  var path = self.MobList[randi() % self.MobList.size()]
 #	return self.Mob
-	pass
+  return load(path)
 
 #func _enter_tree():
 #    connect("", self, "clicked")
 
 func place_mob(mobClass):
-	if (mobClass == null) || (!mobClass.can_instance()):
-		return
-		pass
-	if self.MobCap >= self.TotMobCap:
-		return
-		pass
+  if (mobClass == null) || (!mobClass.can_instance()):
+    return
+    pass
+  if self.MobCap >= self.TotMobCap:
+    return
+    pass
 
-	var mob = mobClass.instance()
-	add_child(mob)
+  var mob = mobClass.instance()
+  add_child(mob)
 
-	
+  
 
 #	var l_x = rand_range(0 + (mob.get_size().x/2), (rect_size.x) - (mob.get_size().x/2))
 #	var l_y = rand_range(0,                        (rect_size.y) - (mob.get_size().y/2))
 
-	var l_x = rand_range(0, rect_size.x)
-	var l_y = rand_range(0, rect_size.y)
+  var l_x = rand_range(0, rect_size.x)
+  var l_y = rand_range(0, rect_size.y)
 
 
-	# no specification means the mob can fly
-	if (self.FlyEnabled && (!mob.has_method("is_ranger") || !mob.is_ranger())):
-		mob.set_position(Vector2(l_x, l_y))
-	else:
-		mob.set_position(Vector2(l_x, rect_size.y))
+  # no specification means the mob can fly
+  if (self.FlyEnabled && (!mob.has_method("is_ranger") || !mob.is_ranger())):
+    mob.set_position(Vector2(l_x, l_y))
+  else:
+    mob.set_position(Vector2(l_x, rect_size.y))
 
-	pass
+  pass
--- projects/SoulsOfAlanis/scene/Potion.gd
+++ projects/SoulsOfAlanis/scene/Potion.gd
@@ -1,11 +1,11 @@
 extends Area2D
 
 func _physics_process(delta):
-	
-	var bodies = get_overlapping_bodies()
-	var item
-	
-	for body in bodies:
-		
-		if body.name == "Player" && Input.is_action_pressed("ui_accept"):
-			$Item.hide()
+  
+  var bodies = get_overlapping_bodies()
+  var item
+  
+  for body in bodies:
+    
+    if body.name == "Player" && Input.is_action_pressed("player_attack"):
+      $Item.hide()
