b9546eaeaee2521d9a0dfcd4e393fdb06be24847
Lucas Eduardo Bretana <lebretana@inf.ufpel.edu.br>
2018-06-26 08:42:49 -0300

    Inital steps for new rooms

--- projects/SoulsOfAlanis/scene/Prelude.gd
+++ projects/SoulsOfAlanis/scene/Prelude.gd
@@ -1,41 +1,41 @@
 extends Node
 
 var path_vector = ["res://scene/HallwayRoom1.tscn", 
-					"res://scene/Corridor1.tscn", 
-					"res://scene/Corridor2.tscn",
-					"res://scene/HallwayRoom2.tscn",
-					"res://scene/LargeRoom1.tscn",
-					"res://scene/LootRoom1.tscn",
-					"res:://scene/LargeRoom1.tscn"]
+          "res://scene/Corridor1.tscn", 
+          "res://scene/Corridor2.tscn",
+          "res://scene/HallwayRoom2.tscn",
+          "res://scene/LargeRoom1.tscn",
+          "res://scene/LootRoom1.tscn",
+          "res:://scene/LargeRoom1.tscn"]
 
 func _ready():
-								
-	$Toucan/AnimatedSprite.play()
-	$Toucan2/AnimatedSprite.play()
-	$Toucan3/AnimatedSprite.play()
-	$Toucan4/AnimatedSprite.play()
-	$Toucan5/AnimatedSprite.play()
-	$Toucan6/AnimatedSprite.play()
-		
+                
+  $Toucan/AnimatedSprite.play()
+  $Toucan2/AnimatedSprite.play()
+  $Toucan3/AnimatedSprite.play()
+  $Toucan4/AnimatedSprite.play()
+  $Toucan5/AnimatedSprite.play()
+  $Toucan6/AnimatedSprite.play()
+    
 func randomize_path():
-	
-	var temp_path = []
-	var temp
-	
-	for i in path_vector.size():
-		randomize()
-		temp_path.append(randi() % path_vector.size())
-		
-	for i in path_vector.size():
-		temp = temp_path[i]
-		temp_path[i] = path_vector[temp]		
+  
+  var temp_path = []
+  var temp
+  
+  for i in path_vector.size():
+    randomize()
+    temp_path.append(randi() % path_vector.size())
+    
+  for i in path_vector.size():
+    temp = temp_path[i]
+    temp_path[i] = path_vector[temp]		
 
-	return temp_path
+  return temp_path
 
 func _on_WarpToNextArea_body_entered(body):
-	
-	var temp_path
-	temp_path = randomize_path()
-	
-	if body.name == "Player":
-		get_tree().change_scene(temp_path.pop_front())
+  
+  var temp_path
+  temp_path = randomize_path()
+  
+  if body.name == "Player":
+    get_tree().change_scene(temp_path.pop_front())
