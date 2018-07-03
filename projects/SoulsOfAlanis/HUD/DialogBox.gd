d2a830f7c002b9bc7981c38e2ed7bf5749242ac2
Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>
2018-07-03 14:56:35 -0300

    Some object orientation

--- projects/SoulsOfAlanis/HUD/DialogBox.gd
+++ projects/SoulsOfAlanis/HUD/DialogBox.gd
@@ -1,39 +1,10 @@
-extends CanvasLayer
-
-enum POSITION {Up, Down}
-export(POSITION) var Position = POSITION.Down
-export var HIGHT = 180
-export var NUMBER_CHAR = 305
-export var MARGIN = 10
-
-signal finished_dialog
+extends "res://HUD/TextBox.gd"
 
 var texts = []
 
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
 		self.next_text()
-	return
-
-func _ready():
-	$NPPainel/RedButton.visible = false
-	$NPPainel/GreenButton.visible = true
-	self.update_position()
-	return
-
-func update_position():
-	$NPPainel.set_margin(0, self.MARGIN)
-	$NPPainel.set_margin(2, - self.MARGIN)
-	if Position == POSITION.Up:
-		$NPPainel.set_anchor(1, 0, false, true)
-		$NPPainel.set_anchor(3, 0, false, true)
-		$NPPainel.set_margin(1, self.MARGIN)
-		$NPPainel.set_margin(3, (self.HIGHT + self.MARGIN))
-	else:
-		$NPPainel.set_anchor(1, 1, false, true)
-		$NPPainel.set_anchor(3, 1, false, true)
-		$NPPainel.set_margin(1, - (self.HIGHT + self.MARGIN))
-		$NPPainel.set_margin(3, - self.MARGIN)
 	return
 
 func set_dialog(name, text):
@@ -62,8 +33,4 @@ func next_text():
 func set_text(text):
 	$NPPainel/Main.clear()
 	$NPPainel/Main.add_text(text)
-	return
-
-func set_speaker(name):
-	$NPPainel/Title.set_text(name + ":")
 	return
---
+++ projects/SoulsOfAlanis/HUD/TextBox.gd
@@ -0,0 +1,34 @@
+extends CanvasLayer
+
+enum POSITION {Up, Down}
+export(POSITION) var Position = POSITION.Down
+export var HIGHT = 180
+export var NUMBER_CHAR = 305
+export var MARGIN = 10
+
+signal finished_dialog
+
+func _ready():
+	$NPPainel/RedButton.visible = false
+	$NPPainel/GreenButton.visible = true
+	self.update_position()
+	return
+
+func update_position():
+	$NPPainel.set_margin(0, self.MARGIN)
+	$NPPainel.set_margin(2, - self.MARGIN)
+	if Position == POSITION.Up:
+		$NPPainel.set_anchor(1, 0, false, true)
+		$NPPainel.set_anchor(3, 0, false, true)
+		$NPPainel.set_margin(1, self.MARGIN)
+		$NPPainel.set_margin(3, (self.HIGHT + self.MARGIN))
+	else:
+		$NPPainel.set_anchor(1, 1, false, true)
+		$NPPainel.set_anchor(3, 1, false, true)
+		$NPPainel.set_margin(1, - (self.HIGHT + self.MARGIN))
+		$NPPainel.set_margin(3, - self.MARGIN)
+	return
+
+func set_speaker(name):
+	$NPPainel/Title.set_text(name + ":")
+	return
