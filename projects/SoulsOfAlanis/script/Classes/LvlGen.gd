ada007c11069f859f96d621b05fc803dca289a09
Lucas Eduardo Bretana <lebretana@inf.ufpel.edu.br>
2018-07-07 03:04:23 -0300

    Solved bug on map generation, and added some extra rules

--- projects/SoulsOfAlanis/script/Classes/LvlGen.gd
+++ projects/SoulsOfAlanis/script/Classes/LvlGen.gd
@@ -2,8 +2,9 @@ extends Button
 
 const TreeMap = preload("res://script/Classes/TreeMap.gd")
 
-const MAX_TRY = 50
+const MAX_TRY = 5
 const N_ROOM  = 15
+const DEF_ROOM = "res://script/Classes/DummyRoom.tscn"
 
 enum RoomType {loot, ordinary, connection, mission, challange, final, any, avoid}
 enum Half { first, second , any }
@@ -13,68 +14,76 @@ var count 	= 0    		# Number of room already picked
 var root  	= null
 
 func ready():
-	randomize()
+  randomize()
 
 # Receives the list of scenes for that level
 func init_rooms(rooms_path = []):
-	printerr("(DB) Initilizing rooms")
-	var f = File.new()
-	var has_conn_room = false
-	# validation
-	for c_room in rooms_path:
-		if !f.file_exists(c_room):
-			printerr(c_room + " does not exists.")
-			exit(1)
+  printerr("(DB) Initilizing rooms")
+  var f = File.new()
+  var has_conn_room = false
+  # validation
+  for c_room in rooms_path:
+    if !f.file_exists(c_room):
+      printerr(c_room + " does not exists.")
+      exit(1)
 
-		var scene = load(c_room).instance()
+    var scene = load(c_room).instance()
 
-		var type = first
-		if scene.has_method("getSceneType"):
-			type = scene.getSceneType()
-			printerr("(DB) Inside LvlGen -> getSceneType")
-			if (type == connection): has_conn_room = true
+    var type = first
+    if scene.has_method("getSceneType"):
+      type = scene.getSceneType()
+      if (type == connection): has_conn_room = true
+    else:
+      printerr("(WW) Couldn't getSceneType, using default")
 
-		var half = first
-		if scene.has_method("getSceneHalf"):
-			half = scene.getSceneHalf()
+    var half = first
+    if scene.has_method("getSceneHalf"):
+      half = scene.getSceneHalf()
+    else:
+      printerr("(WW) Couldn't getSceneHalf, using default")
 
-		var mx_rep = 1
-		if scene.has_method("getMaxRep"):
-			mx_rep = scene.getMaxRep()
+    var mx_rep = 1
+    if scene.has_method("getMaxRep"):
+      mx_rep = scene.getMaxRep()
+    else:
+      printerr("(WW) Couldn't getMaxRep, using default")
 
-		var n_exit = 1
-		if scene.has_method("getNumExit"):
-			mx_rep = scene.getNumExit()
+    var n_exit = 1
+    if scene.has_method("getNumExit"):
+      n_exit = scene.getNumExit()
+    else:
+      printerr("(WW) Couldn't getNumExit, using default")
 
-		# fill up the deck
-		for i in range(0, mx_rep):
-			self.i_rooms.append(InfoRoom.new(c_room, type, half))
+    # fill up the deck
+    # NOTE: possible bug, it looks like (from debug) that the reference to 
+    # room path is the same on every added InfoRoom
+    # 1) it may not be a problem, 
+    #  since is just a reference to the same string path
+    for i in range(0, mx_rep):
+      self.i_rooms.append(InfoRoom.new(c_room, type, half, n_exit))
 
-	if !has_conn_room:
-		printerr("(WW) It is probably necessary that the system have at least one connection room")
-	return
+    pass
+
+  if !has_conn_room:
+    printerr("(WW) It is probably necessary that the system have at least one connection room")
+  return
 
 # Creates the map
-static func createTree(gen, scene, i_rooms = []):
-	printerr("(DB) Creating tree")
-	var f = File.new()
+static func createTree(gen, path_room = DEF_ROOM, i_rooms = []):
+  var f = File.new()
 
-	if (!scene):
-		printerr("(EE) Invalid scene for root node");
-		return null
+  if (!f.file_exists(path_room)) || (!load(path_room).can_instance()):
+    printerr("(EE) Invalid default initial room")
+    return null
 
-#	printerr("(DB) The scenes: ")
-#	for r in rooms:
-#		printerr("(DB) path: %s" % r.scene)
-#		printerr("(DB) type: %s" % r.room_type)
-#		printerr("(DB) half: %s" % r.half)
-#		printerr("(DB) exits %s" % r.n_exit)
+#  printerr("(DB) The scenes: ")
+#  for r in rooms:
+#    printerr("(DB) path: %s" % r.scene);
+#    printerr("(DB) type: %s" % r.room_type)
+#    printerr("(DB) half: %s" % r.half)
+#    printerr("(DB) exits %s" % r.n_exit)
 
-	if (!i_rooms):
-		printerr("(EE) Invalid scenes for the map");
-		return null
-
-	return TreeMap.new(gen, InfoRoom.new(scene, ordinary), null, 0, 1)
+  return TreeMap.new(gen, InfoRoom.new(path_room, ordinary), null, 0, 1)
 
 # Randomly choosen a scene
 # type  : the room type, being the ones defined above
@@ -82,58 +91,67 @@ static func createTree(gen, scene, i_rooms = []):
 # avoid : room type to avoid picking
 # n_try : if the codes tries too much, the it forces a scene to be choosen
 # return : InfoRoom
-func pick(type = any, half = any, avoid = final, n_try = MAX_TRY):
-	if type == RoomType.avoid:
-		printerr("(EE) Cannot select and avoid the same type")
-		avoid = final
-		pass
+func pick(type = any, half = any, avoid = final, n_try = MAX_TRY, force = false):
+  if type == avoid:
+    printerr("(EE) Cannot select and avoid the same type")
+    avoid = final
+  pass
 
-	while (n_try != 0):
-		var i_room = self.i_rooms[randi() % i_rooms.size()]
-		n_try -= 1
-		if ( (type == any) || (i_room.room_type == type) ) && (i_room.room_type != avoid) && ( (half == any) || (i_room.half == half) ):
-			if i_room.room_type != connection && self.i_rooms.size() > 1:
-				self.i_rooms.erase(i_room)
-			self.count += 1
-			return i_room
+  self.count += 1
+  while (n_try != 0):
+    var i_room = self.i_rooms[randi() % i_rooms.size()]
+    n_try -= 1
+    if force || ( ( (type == any) || (i_room.room_type == type) ) && ( i_room.room_type != avoid ) && ( (half == any) || (i_room.half == half) ) ):
+      if i_room.room_type != connection && self.i_rooms.size() > 1:
+        self.i_rooms.erase(i_room)
+      return i_room
+  pass
 
-	self.count += 1
-	printerr("(WW) Could not find the right, requesting a connection one")
-	return self.pick(connection, -1)
+  printerr("(WW) Could not find the right, requesting a connection one, and forcing")
+  return self.pick(connection, any, final, MAX_TRY, true)
 
 func printTree(node, recursive = false):
-	if node.parent == null:
-		print("ROOT!")
-	print("Scene: %s " % node.scene)
-	print("Parent: %s" % node.parent)
-	print("Children: (%s) %s" % [ node.children.size(), node.children])
-	print("High: %s" % node.high)
-	for i in node.children:
-		if recursive:
-			printTree(i, recursive)
-		else:
-			print("Info: " % i)
+  print("------------------------")
+  if node.parent == null:
+    print("ROOT!")
+  print("Scene: %s " % node.i_scene)
+  print(">>Scene: %s - (t: %s, h: %s) e: %s" % [node.i_scene.scene, node.i_scene.room_type, node.i_scene.half, node.i_scene.n_exit])
+  print("Parent: %s" % node.parent)
+  if node.parent != null:
+    print(">>Parent: %s - (t: %s, h: %s) e: %s" % [node.parent.i_scene.scene, node.parent.i_scene.room_type, node.parent.i_scene.half, node.parent.i_scene.n_exit])
+  print("High: %s" % node.high)
+  print("Children: (%s) %s" % [ node.children.size(), node.children])
+  print("|---------------|")
+  for i in node.children:
+    if recursive:
+      printTree(i, recursive)
+    else:
+      print("Info: " % i)
+  print("|---------------|")
+  print("------------------------")
+
 
 func _on_Button_pressed():
-	printerr("(DB) Pressed")
-	var s = PoolStringArray(["res://script/Classes/DummyRoom.tscn", "res://script/Classes/DummyRoom.tscn"])
-	self.init_rooms(s)
-	self.root = createTree(self, load("res://script/Classes/DummyRoom.tscn").instance(), self.i_rooms)
-	printTree(self.root, true)
-	pass # replace with function body
+  printerr("(DB) Pressed")
+  var s = PoolStringArray(["res://script/Classes/DummyRoom.tscn", "res://script/Classes/DummyRoom.tscn"])
+  self.init_rooms(s)
+  self.root = createTree(self, "res://script/Classes/DummyRoom.tscn", self.i_rooms)
+  printTree(self.root, true)
+  printerr("(DB) Succefully generated a #%s rooms map" % self.count)
+  pass # replace with function body
 
 class InfoRoom:
-	#
-	var scene     = null  # scene
-	var room_type = null  # scene type
-	var half      = null  # scene position
-	var n_exit		= 1			# number of exits of the scene
-	##
+  #
+  var scene     = null  # path scene
+  var room_type = null  # scene type
+  var half      = null  # scene position
+  var n_exit	= 1	# number of exits of the scene
+  ##
 
-	# Creates a RoomShell
-	func _init(sc, rt = null, ha = null, ne = 1):
-		self.scene      = sc
-		self.room_type  = rt
-		self.half       = ha
-		self.n_exit     = ne
-		return
+  # Creates a RoomShell
+  func _init(sc, rt = null, ha = null, ne = 1):
+    self.scene      = sc
+    self.room_type  = rt
+    self.half       = ha
+    self.n_exit     = ne
+    return
--- projects/SoulsOfAlanis/script/Classes/TreeMap.gd
+++ projects/SoulsOfAlanis/script/Classes/TreeMap.gd
@@ -1,4 +1,4 @@
-const MIN_HIGH = 3
+const MIN_HIGH = 2#3
 const MAGIC_CONT = 1
 
 enum RoomType {loot = 0, ordinary = 1, connection = 2, mission = 3, challange = 4, final = 5, any = 6}
@@ -7,32 +7,38 @@ enum Half { first = 0, second = 1 , any = 2 }
 # The scene of this node
 var i_scene = null
 
-var i_parent   = null   # Its entry point
-var i_children = []     # Its exists points
-var high	     = 0
+var parent   = null     # Its entry point
+var children = []       # Its exists points
+var high       = 0
+var any_closed = false  # if it a not fully open scene
 
 # shoud be mannually called only for the level generation
 # root, if you will
 func _init(gen, i_sc, p, high, n_exit = 1):
-	# scene must exists, otherwise, what is my purpose?
-	if !i_sc.scene:
-		printerr("no scene. \nwhat is my purpose?")
-		return null
+  #
+  # scene must exists, otherwise, what is my purpose?
+  if !i_sc.scene:
+    printerr("no scene. \nwhat is my purpose?")
+    return null
 
-	self.i_scene 	= i_sc
-	self.i_parent = p
-	self.high 		= high
+  self.i_scene  = i_sc
+  self.parent = p
+  self.high     = high
 
-	for i in range(0, n_exit):
-		if (self.high <= MIN_HIGH):
-			var avoid_type = self.i_scene.room_type
-			var i_new_scene
-			i_new_scene = gen.pick(any, any, avoid_type)
-			self.i_children.append(get_script().new(gen, i_new_scene, self, self.high + 1, 1))
-		else:
-			if (randi() % (MAGIC_CONT * (high - MIN_HIGH)) == 0):
-				var half = any if (self.half == first) else second
-				var i_new_scene = gen.pick(any, half, self.i_scene.room_type)
-				self.i_children.append(new(gen, i_new_scene, self, self.high + 1, 1))
+  printerr("(DB) Generating node high = %s for scene = %s" % [high, i_sc.scene])
 
-		pass
+  for i in range(0, n_exit):
+    printerr("(DB)    generating child %s/%s on high %s" % [i+1, n_exit, high])
+    if (self.high <= MIN_HIGH):
+      printerr("(DB)    per min high (%s)" % self.high)
+      var i_new_scene = gen.pick(any, first, self.i_scene.room_type)
+      self.children.append(get_script().new(gen, i_new_scene, self, self.high + 1, i_new_scene.n_exit))
+    else:
+      if (randi() % (self.high - MIN_HIGH) == 0): # 1/1, 1/2, 1/3, ...
+        var half = any if (self.i_scene.half == Half.first) else Half.second
+        var i_new_scene = gen.pick(any, half, self.i_scene.room_type)
+        self.children.append(get_script().new(gen, i_new_scene, self, self.high + 1, i_new_scene.n_exit))
+      else:
+        print("(DB) Closed room")
+        self.any_closed = true
+        self.children = [] # closed scene
