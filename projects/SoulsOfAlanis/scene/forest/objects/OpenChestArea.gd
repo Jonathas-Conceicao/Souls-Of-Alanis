e5fd47aacd0e3ec4615a387d06491608461b92b3
Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>
2018-07-14 00:19:20 -0300

    Modified Florest Chest to the new model of player interaction

--- projects/SoulsOfAlanis/scene/forest/objects/OpenChestArea.gd
+++ projects/SoulsOfAlanis/scene/forest/objects/OpenChestArea.gd
@@ -2,18 +2,26 @@ extends Area2D
 
 var STATE_CHEST = "CLOSED"
 
-func _input(event):
-	
-	var bodies = get_overlapping_bodies()
-	
-	for body in bodies:
-		
-		if body.name == "Player" && STATE_CHEST == "CLOSED":
-			$Label.show()
-			if event.is_action_pressed("ui_up"):
-				$Open_se.play()
-				$AnimatedSprite.play("opened")
-				$Label.hide()
-				STATE_CHEST = "OPENED"	
-		else:
-			$Label.hide()
+func show_popUp():
+	$Label.show()
+	return
+
+func hide_popUp():
+	$Label.hide()
+	return
+
+func _on_player_interaction(host):
+	if STATE_CHEST == "CLOSED":
+		$Open_se.play()
+		$AnimatedSprite.play("opened")
+		self.hide_popUp()
+		STATE_CHEST = "OPENED"
+	return
+
+func _on_Open_chest_area_body_entered(body):
+	if body.name == "Player" && STATE_CHEST == "CLOSED":
+		self.show_popUp()
+	return
+
+func _on_Open_chest_area_body_exited(body):
+	self.hide_popUp()
