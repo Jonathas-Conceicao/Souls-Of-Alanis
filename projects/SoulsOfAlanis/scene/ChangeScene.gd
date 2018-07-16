f1d8b9820c354c12e0b6fdd6a77e01df05415a0a
Lucas Eduardo Bretana <lebretana@inf.ufpel.edu.br>
2018-07-16 02:55:24 -0300

    Better in/out markers

--- projects/SoulsOfAlanis/scene/ChangeScene.gd
+++ projects/SoulsOfAlanis/scene/ChangeScene.gd
@@ -1,5 +1,15 @@
 extends Area2D
 
+export var blocked = false
 
 func _get_exit():
 	return -1
+
+func _on_EntryPoint_body_entered(body):
+	if !self.blocked:
+		$InteractionSign.visible = true
+	return
+
+func _on_EntryPoint_body_exited(body):
+	$InteractionSign.visible = false
+	return
--- projects/SoulsOfAlanis/script/control/Exit.gd
+++ projects/SoulsOfAlanis/script/control/Exit.gd
@@ -1,12 +1,13 @@
 extends Area2D
 
 export (int) var ExitIndex = 0
+export (bool) var Blocked = false
 
 func _get_exit():
 	return self.ExitIndex
 
 func _on_Exit_body_entered(body):
-	if body.get_name() == "Player":
+	if !self.Blocked && body.get_name() == "Player":
 		$InteractionSign.show()
 
 
