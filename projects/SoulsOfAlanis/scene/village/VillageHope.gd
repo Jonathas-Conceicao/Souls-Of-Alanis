70b1b33c419fce17f2a9de18b12b8b0bdc9abfb4
Asaki <asakizin@gmail.com>
2018-07-15 21:29:48 -0300

    small changes

--- projects/SoulsOfAlanis/scene/village/VillageHope.gd
+++ projects/SoulsOfAlanis/scene/village/VillageHope.gd
@@ -2,6 +2,10 @@ extends Node2D
 
 func _ready():
 	$LocalName.show()
+	#get_tree().get_current_scene().get_node("Player").get_node("Camera2D").set_limit(MARGIN_LEFT, 0)
+	#get_tree().get_current_scene().get_node("Player").get_node("Camera2D").set_limit(MARGIN_RIGHT, 2880)
+	#get_tree().get_current_scene().get_node("Player").get_node("Camera2D").set_limit(MARGIN_BOTTOM, 1152)
+	#get_tree().get_current_scene().get_node("Player").get_node("Camera2D").set_limit(MARGIN_TOP, 0)
 
 func getSize():
 	return Vector2(2, 4)
