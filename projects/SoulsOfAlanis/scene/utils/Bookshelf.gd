16b69516f9a8f991ee3605d43fcd6669cb46f59f
Lucas E. Bretana <lucasbretana@gmail.com>
2018-07-17 18:31:06 -0300

    working on util scenes

--- projects/SoulsOfAlanis/scene/utils/Bookshelf.gd
+++ projects/SoulsOfAlanis/scene/utils/Bookshelf.gd
@@ -1,5 +1,16 @@
 extends "res://script/Classes/Unique.gd"
 
+# disables the Unique
+func use():
+	debug.printMsg("Implement a \"use\" function", debug.msg_type.err)
+	self.enabled(false)
+	return
+
+# drops a random item
+func drop():
+	debug.printMsg("Implement a \"drop\" function", debug.msg_type.wrn)
+	return
+
 # define by @Jonathas on #1489c0194c6679ffb9a2fd2535a71261e958245f
 func get_uniqueID():
 	return global_ids.unique_ids.breakable_bookshelf
