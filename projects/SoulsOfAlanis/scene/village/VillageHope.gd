d391fdcf905b149c206ba2bd99d664e5ef5530fb
Asaki <asakizin@gmail.com>
2018-07-18 14:08:59 -0300

    small changes

--- projects/SoulsOfAlanis/scene/village/VillageHope.gd
+++ projects/SoulsOfAlanis/scene/village/VillageHope.gd
@@ -2,11 +2,14 @@ extends Node2D
 
 func _input(event):
 	if event.is_action_pressed("disabled_NPC"):
-		$GhostBill.set_disabled()
-		$Andre.set_disabled()
-		$OldThief.set_disabled()
+		$GhostBill.set_enabled(false)
+		$Andre.set_enabled(false)
+		$OldThief.set_enabled(false)
 		
 	if event.is_action_pressed("enabled_NPC"):
-		$GhostBill.set_enabled()
-		$Andre.set_enabled()
-		$OldThief.set_enabled()
+		$GhostBill.set_enabled(true)
+		$Andre.set_enabled(true)
+		$OldThief.set_enabled(true)
+		
+	if event.is_pressed():
+		$LocalName.hide()
