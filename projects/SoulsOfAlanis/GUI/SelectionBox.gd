80eeadb2a58cec4a3249339e0f5948c49b6d17ae
Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>
2018-07-18 12:11:52 -0300

    Propper interface and example for setting the SelectionBox

--- projects/SoulsOfAlanis/GUI/SelectionBox.gd
+++ projects/SoulsOfAlanis/GUI/SelectionBox.gd
@@ -5,11 +5,24 @@ const ItemSelection = preload("ItemSelection.tscn")
 var itemList
 var selected
 
+func test_ready():
+	self.set_dialog("Speaker", "A long text telling a how story about the amazing number of options you can  choose in this dialog box")
+	self.add_item("Yes")
+	self.add_item("No")
+	self.add_item("Maybe")
+	self.add_item("No way, hosay!")
+	self.enabeled(true)
+	return
+
 func _ready():
-	self.itemList = $NPPainel/ItemList.get_children()
-	if self.itemList:
-		self.itemList[0].selected(true)
-	self.selected = 0
+	self.update_selected()
+
+	# self.test_ready()
+	return
+
+func showItens():
+	$NPPainel/ItemList.visible = true
+	self.update_selected()
 	return
 
 func _input(event):
@@ -17,8 +30,13 @@ func _input(event):
 		self.select_next_item()
 	elif event.is_action_pressed("ui_left"):
 		self.select_prev_item()
-	elif event.is_action_pressed("ui_accept"):
-		self.select_current_item()
+	return
+
+func update_selected():
+	self.itemList = $NPPainel/ItemList.get_children()
+	if self.itemList:
+		self.itemList[0].selected(true)
+	self.selected = 0
 	return
 
 func select_next_item():
@@ -43,7 +61,6 @@ func select_prev_item():
 
 func select_current_item():
 	emit_signal("finished_dialog", self, selected)
-	clear_items()
 	return
 
 func add_item(text):
@@ -56,4 +73,12 @@ func add_item(text):
 func clear_items():
 	for item in $NPPainel/ItemList.get_children():
 		item.queue_free()
+	return
+
+func _on_SelectionBox_finished_dialog(obj):
+	if $NPPainel/ItemList.visible:
+		print("Option selected: ", self.selected)
+		self.select_current_item()
+	else:
+		self.showItens()
 	return
