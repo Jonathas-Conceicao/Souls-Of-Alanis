0dfc9b2a97386499eef9cbb6af121db524e75697
Lucas E. Bretana <lucasbretana@gmail.com>
2018-07-18 18:13:22 -0300

    Better debug, and modification on base scene

--- projects/SoulsOfAlanis/script/Classes/Scene.gd
+++ projects/SoulsOfAlanis/script/Classes/Scene.gd
@@ -1,5 +1,23 @@
 extends Node2D
 
+enum RoomType {loot, ordinary, connection, mission, challenge, final, any, avoid}
+enum Half { first, second , any }
+
+func getSceneType():
+	return ordinary
+
+func getSceneHalf():
+	return first
+
+func getMaxRep():
+	return 10
+
+func getNumExit():
+	return 1
+
+func getSize():
+	return Vector2(1, 1)
+
 func listNPC():
 	return []
 
