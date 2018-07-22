6e350210109c5f32f0feea858ac2d620a6f20574
Lucas Eduardo Bretana <lebretana@inf.ufpel.edu.br>
2018-07-22 04:25:50 -0300

    Major improvment for menu (suck it dep.)

--- projects/SoulsOfAlanis/scene/menu/MainMenu.gd
+++ projects/SoulsOfAlanis/scene/menu/MainMenu.gd
@@ -1,4 +1,4 @@
-extends VBoxContainer
+extends Control
 
 export (PackedScene) var CREDIT_SCENE = null
 export (PackedScene) var StartScene = null#preload("res://scene/control/root.tscn").instance()
@@ -11,9 +11,14 @@ func _on_Credits_pressed():
 	return
 
 func _on_Exit_pressed():
-	print("WTF")
+	get_tree().quit()
 	return
 
 func _on_NewGame_pressed():
-	
+	$FadeIn.show()
+	$FadeIn.fade_in()
+	return
+
+func _on_FadeIn_fade_finished():
+	get_tree().change_scene_to(StartScene)
 	return
