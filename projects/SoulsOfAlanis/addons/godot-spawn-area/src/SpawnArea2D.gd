4953b57b4ba7df1bccac9b7397d651f0ceb7ff1a
Lucas Eduardo Bretana <lebretana@inf.ufpel.edu.br>
2018-07-23 03:51:45 -0300

    Removed error from design time

--- projects/SoulsOfAlanis/addons/godot-spawn-area/src/SpawnArea2D.gd
+++ projects/SoulsOfAlanis/addons/godot-spawn-area/src/SpawnArea2D.gd
@@ -28,29 +28,32 @@ func pick_rand():
 #    connect("", self, "clicked")
 
 func place_mob(mobClass):
-  if (mobClass == null) || (!mobClass.can_instance()):
-    return
-    pass
-  if self.MobCap >= self.TotMobCap:
-    return
-    pass
+	if (mobClass == null) || (!mobClass.can_instance()):
+		return
+	if self.MobCap >= self.TotMobCap:
+		return
 
-  var mob = mobClass.instance()
-  add_child(mob)
-  var player = get_tree().get_root().get_node("root").get_node("Player")
-  mob.connect("StateChanged", player, "_on_creatureStateeChanged")
+	var mob = mobClass.instance()
+	add_child(mob)
+## error protection for design time
+	var treeroot = get_tree().get_root()
+	if !treeroot:
+		var rootnode = treeroot.get_node("root")
+		if !rootnode:
+			var player = rootnode.get_node("Player")
+			if !player:
+				mob.connect("StateChanged", player, "_on_creatureStateeChanged")        
 
 #	var l_x = rand_range(0 + (mob.get_size().x/2), (rect_size.x) - (mob.get_size().x/2))
 #	var l_y = rand_range(0,                        (rect_size.y) - (mob.get_size().y/2))
 
-  var l_x = rand_range(0, rect_size.x)
-  var l_y = rand_range(0, rect_size.y)
+	var l_x = rand_range(0, rect_size.x)
+	var l_y = rand_range(0, rect_size.y)
 
 
   # no specification means the mob can fly
-  if (self.FlyEnabled && (!mob.has_method("is_ranger") || !mob.is_ranger())):
-    mob.set_position(Vector2(l_x, l_y))
-  else:
-    mob.set_position(Vector2(l_x, rect_size.y))
-
+	if (self.FlyEnabled && (!mob.has_method("is_ranger") || !mob.is_ranger())):
+		mob.set_position(Vector2(l_x, l_y))
+	else:
+		mob.set_position(Vector2(l_x, rect_size.y))
   pass
